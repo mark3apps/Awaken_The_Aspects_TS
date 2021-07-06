f52a6f926a4ec6e49947ac39ab352420c9f1e48f
mark3apps <mark.wright.vfx@gmail.com>
2021-07-06 01:53:30 -0700

    Added a bunch more stuff

---
+++ src/app/globals/abilityGlobals.ts
@@ -0,0 +1,19 @@
+/** @noSelfInFile **/
+
+import { Ability } from "app/classes/ability"
+import { OrderId } from "w3ts/globals/order"
+import { ABL } from "./keys"
+
+ABL.bonusCriticalStrike = new Ability("bonusCriticalStrike", "Z003")
+ABL.bonusDamage = new Ability("bonusDamage", "Z004")
+ABL.bonusEvasion = new Ability("bonusEvasion", "Z005")
+ABL.bonusHealthRegen = new Ability("bonusHealthRegen", "Z006")
+ABL.bonusLifeSteal = new Ability("bonusLifeSteal", "Z007")
+ABL.bonusMagicResistance = new Ability("bonusMagicResistance", "Z008")
+ABL.bonusMovementSpeed = new Ability("bonusMovementSpeed", "Z009")
+ABL.bonusStats = new Ability("bonusStats", "Z010")
+ABL.bonusManaRegen = new Ability("bonusManaRegen", "Z011")
+
+
+
+
