0eac7eace071e778f001a0f615357792c1371232
Mark Wright <mark.wright.vfx@gmail.com>
2021-12-03 22:40:42 -0800

    Fixed issues in Leaderboard

--- src/lib/w3ts/handles/leaderboard.ts
+++ src/lib/w3ts/handles/leaderboard.ts
@@ -1,7 +1,7 @@
 /** @noSelfInFile **/
 
-import { Handle } from "./handle";
-import { MapPlayer } from "./player";
+import { Handle } from "./handle"
+import { MapPlayer } from "./player"
 
 export class Leaderboard extends Handle<leaderboard> {
   /**
@@ -11,117 +11,117 @@ export class Leaderboard extends Handle<leaderboard> {
    */
   constructor() {
     if (Handle.initFromHandle()) {
-      super();
+      super()
     } else {
-      super(CreateLeaderboard());
+      super(CreateLeaderboard())
     }
   }
 
   public addItem(label: string, value: number, p: MapPlayer) {
-    LeaderboardAddItem(this.handle, label, value, p.handle);
+    LeaderboardAddItem(this.handle, label, value, p.handle)
   }
 
   public clear() {
-    LeaderboardClear(this.handle);
+    LeaderboardClear(this.handle)
   }
 
   public destroy() {
-    DestroyLeaderboard(this.handle);
+    DestroyLeaderboard(this.handle)
   }
 
   public display(flag: boolean = true) {
-    LeaderboardDisplay(this.handle, flag);
+    LeaderboardDisplay(this.handle, flag)
   }
 
   public get displayed() {
-    return IsLeaderboardDisplayed(this.handle);
+    return IsLeaderboardDisplayed(this.handle)
   }
 
   public get itemCount() {
-    return LeaderboardGetItemCount(this.handle);
+    return LeaderboardGetItemCount(this.handle)
   }
 
   public set itemCount(count: number) {
-    LeaderboardSetSizeByItemCount(this.handle, count);
+    LeaderboardSetSizeByItemCount(this.handle, count)
   }
 
   public getPlayerIndex(p: MapPlayer) {
-    return LeaderboardGetPlayerIndex(this.handle, p.handle);
+    return LeaderboardGetPlayerIndex(this.handle, p.handle)
   }
 
   public hasPlayerItem(p: MapPlayer) {
-    LeaderboardHasPlayerItem(this.handle, p.handle);
+    LeaderboardHasPlayerItem(this.handle, p.handle)
   }
 
   public removeItem(index: number) {
-    LeaderboardRemoveItem(this.handle, index);
+    LeaderboardRemoveItem(this.handle, index)
   }
 
   public removePlayerItem(p: MapPlayer) {
-    LeaderboardRemovePlayerItem(this.handle, p.handle);
+    LeaderboardRemovePlayerItem(this.handle, p.handle)
   }
 
   public setItemLabel(item: number, label: string) {
-    LeaderboardSetItemLabel(this.handle, item, label);
+    LeaderboardSetItemLabel(this.handle, item, label)
   }
 
   public setItemLabelColor(item: number, red: number, green: number, blue: number, alpha: number) {
-    LeaderboardSetItemLabelColor(this.handle, item, red, green, blue, alpha);
+    LeaderboardSetItemLabelColor(this.handle, item, red, green, blue, alpha)
   }
 
   public setItemStyle(item: number, showLabel: boolean = true, showValues: boolean = true, showIcons: boolean = true) {
-    LeaderboardSetItemStyle(this.handle, item, showLabel, showValues, showIcons);
+    LeaderboardSetItemStyle(this.handle, item, showLabel, showValues, showIcons)
   }
 
   public setItemValue(item: number, value: number) {
-    LeaderboardSetItemValue(this.handle, item, value);
+    LeaderboardSetItemValue(this.handle, item, value)
   }
 
   public setItemValueColor(item: number, red: number, green: number, blue: number, alpha: number) {
-    LeaderboardSetItemValueColor(this.handle, item, red, green, blue, alpha);
+    LeaderboardSetItemValueColor(this.handle, item, red, green, blue, alpha)
   }
 
   public setLabelColor(red: number, green: number, blue: number, alpha: number) {
-    LeaderboardSetLabelColor(this.handle, red, green, blue, alpha);
+    LeaderboardSetLabelColor(this.handle, red, green, blue, alpha)
   }
 
   public setPlayerBoard(p: MapPlayer) {
-    PlayerSetLeaderboard(p.handle, this.handle);
+    PlayerSetLeaderboard(p.handle, this.handle)
   }
 
   public setStyle(showLabel: boolean = true, showNames: boolean = true, showValues: boolean = true, showIcons: boolean = true) {
-    LeaderboardSetStyle(this.handle, showLabel, showNames, showValues, showIcons);
+    LeaderboardSetStyle(this.handle, showLabel, showNames, showValues, showIcons)
   }
 
   public setValueColor(red: number, green: number, blue: number, alpha: number) {
-    LeaderboardSetValueColor(this.handle, red, green, blue, alpha);
+    LeaderboardSetValueColor(this.handle, red, green, blue, alpha)
   }
 
   public sortByLabel(asc: boolean = true) {
-    LeaderboardSortItemsByLabel(this.handle, asc);
+    LeaderboardSortItemsByLabel(this.handle, asc)
   }
 
   public sortByPlayer(asc: boolean = true) {
-    LeaderboardSortItemsByPlayer(this.handle, asc);
+    LeaderboardSortItemsByPlayer(this.handle, asc)
   }
 
   public sortByValue(asc: boolean = true) {
-    LeaderboardSortItemsByValue(this.handle, asc);
+    LeaderboardSortItemsByValue(this.handle, asc)
   }
 
   public set label(value: string) {
-    LeaderboardSetLabel(this.handle, value);
+    LeaderboardSetLabel(this.handle, value)
   }
 
   public get label() {
-    return LeaderboardGetLabelText(this.handle);
+    return LeaderboardGetLabelText(this.handle)
   }
 
   public static fromHandle(handle: leaderboard): Leaderboard {
-    return this.getObject(handle);
+    return this.getObject(handle)
   }
 
   public static fromPlayer(p: MapPlayer) {
-    return this.fromHandle(PlayerGetLeaderboard(p.handle));
+    return this.fromHandle(PlayerGetLeaderboard(p.handle))
   }
 }
